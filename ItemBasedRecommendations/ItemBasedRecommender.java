5ebbedd74969d001f9a3793053f29b9b4298fc24
sumandeepk <ksumandeep70@yahoo.com>
2016-04-10 19:07:18 -0700

    Create ItemBasedRecommender.java

---
+++ ItemBasedRecommendations/ItemBasedRecommender.java
@@ -0,0 +1,34 @@
+package com.recommender.collaberativeRecommender;
+import java.io.File;
+import java.util.List;
+import org.apache.mahout.cf.taste.impl.common.LongPrimitiveIterator;
+import org.apache.mahout.cf.taste.impl.model.file.FileDataModel;
+import org.apache.mahout.cf.taste.impl.recommender.GenericItemBasedRecommender;
+import org.apache.mahout.cf.taste.impl.similarity.LogLikelihoodSimilarity;
+import org.apache.mahout.cf.taste.impl.similarity.PearsonCorrelationSimilarity;
+import org.apache.mahout.cf.taste.model.DataModel;
+import org.apache.mahout.cf.taste.recommender.RecommendedItem;
+import org.apache.mahout.cf.taste.similarity.ItemSimilarity;
+public class ItemBasedRecommender {
+public static void main(String arg[]) {
+DataModel model;
+try {
+			model = new FileDataModel(new File("data/movie.csv"));
+ItemSimilarity similarity = new PearsonCorrelationSimilarity(model);;
+GenericItemBasedRecommender recommender = new GenericItemBasedRecommender(model, similarity); 
+//			for (LongPrimitiveIterator iterator = model.getItemIDs(); iterator.hasNext();) {
+//				Long itemId = iterator.nextLong();
+//				List<RecommendedItem> recommendations = recommender.mostSimilarItems(32, 5);
+//				for (RecommendedItem recommendation : recommendations) {
+//					System.out.println(recommendation);
+//				}
+//			}
+//			
+List<RecommendedItem> recommendations = recommender.recommend(913, 5);
+for (RecommendedItem recommendation : recommendations) {
+System.out.println(recommendation);
+			}
+		} catch (Exception e) {
+		}
+	}
+}
