d114bed6a6dc769a049471f8186b0a989b5fd562
sumandeepk <ksumandeep70@yahoo.com>
2016-04-14 22:27:55 -0700

    DataBase Connection

---
+++ ItemBasedRecommendations/DBConnection.java
@@ -0,0 +1,35 @@
+package com.recommender.collaberativeRecommender;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DBConnection {
+	private static Connection con;
+
+	private static final String Driver = "com.mysql.jdbc.Driver";
+
+	private static final String ConnectionString = "jdbc:mysql://localhost/sys?useSSL=false";
+
+	private static final String user = "root";
+
+	private static final String pwd = "";
+
+	public static Connection loadDriver() throws SQLException {
+		try {
+
+			Class.forName(Driver);
+
+		} catch (ClassNotFoundException ex) {
+
+			System.out.println(ex.getMessage());
+
+		}
+
+		con = DriverManager.getConnection(ConnectionString, user, pwd);
+
+		return con;
+
+	}
+
+}
